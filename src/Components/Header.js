8d10dd2feb9dd68a915ea9a792d398d493894e2a
Gyuri Kim <artgr2062@konkuk.ac.kr>
2021-06-17 20:45:11 +0900

    #3.4 Location Aware Header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {Link} from "react-router-dom";
+import { Link, withRouter } from "react-router-dom";
 import styled from "styled-components";
 
 const Header = styled.header`
@@ -11,11 +11,9 @@ const Header = styled.header`
   height: 50px;
   display: flex;
   align-items: center;
-  padding: 0 10px;
   background-color: rgba(20, 20, 20, 0.8);
   z-index: 10;
   box-shadow: 0px 1px 5px 2px rgba(0, 0, 0, 0.8);
-
 `;
 
 const List = styled.ul`
@@ -23,9 +21,12 @@ const List = styled.ul`
 `;
 
 const Item = styled.li`
-  width: 50px;
+  width: 80px;
   height: 50px;
   text-align: center;
+  border-bottom: 3px solid
+    ${(props) => (props.current ? "#c0392b" : "transparent")};
+  transition: border-bottom .5s ease-in-out;
 `;
 
 const SLink = styled(Link)`
@@ -35,22 +36,20 @@ const SLink = styled(Link)`
   justify-content: center;
 `;
 
-function header() {
-  return (
-    <Header>
-      <List>
-        <Item>
-          <SLink to="/">Movies</SLink>
-        </Item>
-        <Item>
-          <SLink to="/tv">TV</SLink>
-        </Item>
-        <Item>
-          <SLink to="/search">Search</SLink>
-        </Item>
-      </List>
-    </Header>
-  );
-}
+const header = ({location: { pathname }}) => (
+  <Header>
+    <List>
+      <Item current={pathname === '/'}>
+        <SLink to="/">Movies</SLink>
+      </Item>
+      <Item current={pathname === '/tv'}>
+        <SLink to="/tv">TV</SLink>
+      </Item>
+      <Item current={pathname === '/search'}>
+        <SLink to="/search">Search</SLink>
+      </Item>
+    </List>
+  </Header>
+);
 
-export default header;
+export default withRouter(header);
