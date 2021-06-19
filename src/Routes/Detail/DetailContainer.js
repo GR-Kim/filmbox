ddd2b7c40362b791d0cf10582cdee790eb3c193a
Gyuri Kim <artgr2062@konkuk.ac.kr>
2021-06-19 16:42:20 +0900

    #5.6 Detail Container part One

--- src/Routes/Detail/DetailContainer.js
+++ src/Routes/Detail/DetailContainer.js
@@ -5,18 +5,25 @@ class DetailContainer extends React.Component {
   state = {
     result: null,
     loading: true,
-    error: null
+    error: null,
   };
+
+  async componentDidMount() {
+    const {
+      match: {
+        params: { id },
+      },
+      history: { push }
+    } = this.props;
+    const parsedID = Number(id);
+    if (isNaN(parsedID)) {
+      return push("/");
+    }
+  }
 
   render() {
     const { result, loading, error } = this.state;
-    return (
-      <DetailPresenter
-        result={result}
-        loading={loading}
-        error={error}
-      />
-    );
+    return <DetailPresenter result={result} loading={loading} error={error} />;
   }
 }
 
