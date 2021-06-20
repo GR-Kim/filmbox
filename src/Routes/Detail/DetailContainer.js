1be3d5aa3845189526697a1f7619fc91d5c487ff
Gyuri Kim <artgr2062@konkuk.ac.kr>
2021-06-20 14:55:32 +0900

    #5.7 Destructuring assignment with let

--- src/Routes/Detail/DetailContainer.js
+++ src/Routes/Detail/DetailContainer.js
@@ -1,23 +1,47 @@
 import React from "react";
+import { moviesApi, tvApi } from "../../api";
 import DetailPresenter from "./DetailPresenter";
 
 class DetailContainer extends React.Component {
-  state = {
-    result: null,
-    loading: true,
-    error: null,
-  };
+  constructor(props) {
+    super(props);
+    const {
+      location: { pathname },
+    } = props;
+    this.state = {
+      result: null,
+      error: null,
+      loading: true,
+      isMovie: pathname.includes("/movie/"),
+    };
+  }
 
   async componentDidMount() {
     const {
       match: {
         params: { id },
       },
-      history: { push }
+      history: { push },
     } = this.props;
+    const { isMovie } = this.state;
     const parsedID = Number(id);
     if (isNaN(parsedID)) {
       return push("/");
+    }
+    //왜 const로 받으면 안되지?
+    let result = null;
+    try{
+      if(isMovie){
+        //data를 받으면 되는데 이를 result라는 이름으로 받음.
+        ({data: result} = await moviesApi.movieDetail(parsedID));
+      } else{
+        ({data: result} = await tvApi.showDetail(parsedID));
+      }
+      
+    }catch{
+      this.setState({error: "Can't find anything"});
+    }finally{
+      this.setState({loading: false, result});
     }
   }
 
