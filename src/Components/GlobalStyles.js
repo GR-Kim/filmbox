1875ec08772c8b8dfaa1ada23ab4fdacae9fe3f4
Gyuri Kim <artgr2062@konkuk.ac.kr>
2021-06-20 17:05:28 +0900

    #6.1 HomePresenter and Section Components

--- src/Components/GlobalStyles.js
+++ src/Components/GlobalStyles.js
@@ -14,8 +14,10 @@ const GlboalStyles = createGlobalStyle`
         font-family: --apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
         font-size: 12px;
         background-color: rgba(20, 20, 20, 1);
+        // body 아래 요소까지 흰 글씨로 됨.
         color: white;
-        padding-top: 50px;
+        //fixed된 헤드 부분과 겹치지 않기 위해
+        padding-top: 80px;
     }
 
 `;
